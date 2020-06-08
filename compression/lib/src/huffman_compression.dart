e0f7ef26cd161e448bee92f0eb45d61faf919c39
Mathilde Chabeau <mathilde.chabeau@epitech.eu>
2020-06-08 16:03:10 +0200

    binary code by color

--- compression/lib/src/huffman_compression.dart
+++ compression/lib/src/huffman_compression.dart
@@ -1,14 +1,8 @@
-import 'dart:typed_data';
-
 import 'package:picts_manager_huffman_compression/src/utils/image_utils.dart';
 
 String huffmanCompression()
 {
   ImageUtils imageUtils = new ImageUtils("./img/lenna.jpg");
   imageUtils.readImage();
-  /*print(ByteData(0));
-  print(ByteData(1));
-  print(ByteData(2));
-  print(ByteData(3));*/
   return "HUFFMAN COMPRESSION";
 }
--- compression/lib/src/utils/image_utils.dart
+++ compression/lib/src/utils/image_utils.dart
@@ -7,17 +7,19 @@ class ImageUtils
   Map colorsProbability;
   final String imagePath;
   Image img;
+  Map colorsBinaryCode;
 
   ImageUtils(this.imagePath)
   {
     this.colorsProbability = new HashMap();
+    this.colorsBinaryCode = new HashMap();
   }
 
   void readImage()
   {
     this.img = decodeJpg(File(this.imagePath).readAsBytesSync());
     this.fillSortedProbabilities();
-    //print("IMAGE : ");
+    this.createColorsBinaryEquivalent();
   }
 
   void fillSortedProbabilities()
@@ -42,12 +44,12 @@ class ImageUtils
       }
     }
     this.colorsProbability.forEach((k,v) => this.colorsProbability.update(k, (dynamic v) => v/totalPixels*100));
-    this.colorsProbability = this.sortMapByValues();
+    this.colorsProbability = this.sortMapByValuesDesc();
     //this.colorsProbability.forEach((k,v) => print('${k}: ${v}'));
     //print(totalPixels);
   }
 
-  Map sortMapByValues()
+  Map sortMapByValuesAsc()
   {
     var sortedEntries = this.colorsProbability.entries.toList()..sort((e1, e2) {
       var diff = e1.value.compareTo(e2.value);
@@ -56,5 +58,27 @@ class ImageUtils
     });
 
     return (Map.fromEntries(sortedEntries));
+  }
+
+  Map sortMapByValuesDesc()
+  {
+    var sortedEntries = this.colorsProbability.entries.toList()..sort((e1, e2) {
+      var diff = e2.value.compareTo(e1.value);
+      if (diff == 0) diff = e2.key.compareTo(e1.key);
+      return diff;
+    });
+
+    return (Map.fromEntries(sortedEntries));
+  }
+
+  void createColorsBinaryEquivalent()
+  {
+    int base = 2;
+    List keys = this.colorsProbability.keys.toList();
+
+    for (int i = 1; i <= keys.length; i++) {
+      this.colorsBinaryCode[keys[i-1]] = i.toRadixString(base);
+    }
+    this.colorsBinaryCode.forEach((k,v) => print('${k}: ${v}'));
   }
 }
